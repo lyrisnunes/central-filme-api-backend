8da4dca0403486ea4368812d0877dac298bfffe9
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-09-30 09:01:16 -0300

    Atualização

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,24 +1,17 @@
 const {hash, compare}  = require("bcryptjs");
 
 const AppError = require("../utils/AppError");
+const UserRepository = require("../repositories/UserRepository");
 const sqliteConnection = require("../database/sqlite");
+const UserCreateService = require("../services/UserCreateService");
 
 class UsersControllers{
   async create(request, response){
       const {name, email, password} = request.body;
 
-      const database = await sqliteConnection();
-      const checkUsersExists = await database.get("SELECT * FROM users WHERE email =(?)", [email])
-
-      if(checkUsersExists){
-         throw new AppError("Este email já esta sendo usado.")
-      }
-
-      const hashedPassword = await hash(password, 8)
-
-      await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)", 
-      [name, email, hashedPassword]
-      );
+      const userRepository = new UserRepository();
+      const userCreateService = new UserCreateService(userRepository);
+      await userCreateService.execute({name, email, password});
 
        return response.status(201).json();
    }
---
+++ src/services/UserCreateService.js
@@ -0,0 +1,23 @@
+const {hash, compare}  = require("bcryptjs");
+const AppError = require("../utils/AppError");
+
+class UserCreateService {
+  constructor(userRepository) {
+   this.userRepository = userRepository;
+
+  }
+
+  async execute({ name, email, password }) {
+    const checkUsersExists = await this.userRepository.findByEmail(email);
+
+    if (checkUsersExists) {
+      throw new AppError("Este email já esta sendo usado.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.userRepository.create({ name, email, password: hashedPassword });
+  }
+}
+
+module.exports = UserCreateService;
